a0b558eb37d9db363330e1603e83470724337841
ethan92429 <ethan.keller@gmail.com>
2016-04-07 23:05:59 -0400

    Have DB working wtih localstorage... played around with jaydata and persistence but to no avail

--- www/js/db.js
+++ www/js/db.js
@@ -1,31 +1,62 @@
-$data.Entity.extend("Todo", {
-    Id: { type: "int", key: true, computed: true },
-    Task: { type: String, required: true, maxLength: 200 },
-    DueDate: { type: Date },
-    Completed: { type: Boolean }
-});
+// This "DB" uses the simple key-value pair storage known as local Storage. For now, local storage
+// provides a simple way to store two weeks worth of data. If we decide to store more, or for whatever
+// reason local storage doesn't work, perhaps we should move to a SQLite plugin within PhoneGap. TBD
 
-$data.EntityContext.extend("TodoDatabase", {
-    Todos: { type: $data.EntitySet, elementType: Todo }
-});
+
+// Start everything up
 $(document).ready(onDeviceReady())
-// PhoneGap is ready
-//
 function onDeviceReady() {
-  var todoDB = new TodoDatabase({ 
-    provider: 'webSql', databaseName: 'MyTodoDatabase'
-	});
-	todoDB.onReady(function() {
-	  $("#first_name").on("change", function(){
-	  	var tasks = todoDB.Todos.addMany([
-	    	{ Task: $("#first_name").val(), Completed: true },
-	    	{ Task: 'Step1: Define your data model'},
-	    	{ Task: 'Step2: Initialize data storage'}
-			]);
-			todoDB.saveChanges(function() {
-			  tasks.forEach( function(todo) { alert(todo.Id) });
-			});
-		})
+	deleteOldPlantData();
+	updatePlantData();
+}
+
+// Inserts a list of plant data records into local storage
+function insertManyPlantData(plantData) {
+	for (i in plantData) {
+		setPlantDataRecord(plantData[i])
+	}
+}
+
+function retrieveAllPlantData() {
+	var maxKey = localStorage.getItem('maxKey');
+	var minKey = localStorage.getItem('minKey');
+	var plantData = [];
+	for (i = minKey; i < maxKey; i++) {
+		plantData[i-minKey]=localStorage.getItem(i);
+	}
+	return plantData
+}
+
+// Put the plant data record into storage
+function setPlantDataRecord(plantDataRecord) {
+	localStorage.setItem(plantDataRecord.pid, JSON.stringify(plantDataRecord));
+}
+
+// MetaStats is an object that will store all of the persistent data, such as 
+// plant data, and the range of keys currently held in local storage
+function initializeMetaStats(json) {
+	var plantName = askForPlantName();
+	localStorage.setItem('plantName', plantName);
+	localStorage.setItem('minKey', json[0].pid);
+	localStorage.setItem('maxKey', json[json.length-1].pid);
+}
+function writeMetaStats(json) {
+	var minKey = localStorage.getItem('minKey');
+	if (minKey == null) {
+		initializeMetaStats(json);
+	}
+	else {
+		localStorage.setItem('maxKey', json[json.length-1].pid);
+	}
+}
+
+function updatePlantData(latestPlantDataKey){
+	$.getJSON("past_data_shortened.json", function(json) {
+		insertManyPlantData(json);
+		writeMetaStats(json);
 	});
 }
 
+//TODO Functions
+function askForPlantName(){};
+function deleteOldPlantData(){};
