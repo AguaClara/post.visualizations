81ea4075fb8d708e529335db2e242240d9f21bea
Shangyuan Niu <sn522@cornell.edu>
2016-04-16 14:42:37 -0400

    Put the table into a separate page

--- www/js/table.js
+++ www/js/table.js
@@ -1,14 +1,14 @@
 function settable(data){
   var tabledata = data;
-  var currenttime = new Date("2016-02-03");
+  var currenttime = new Date(Date.now());
   var datebound = new Date(currenttime.setDate(currenttime.getDate()-7));
   var table = d3.select("#table").append("table");
-  var entry = data.pop();
+  var entry = tabledata.pop();
   var roll = table.append("tr");
   for(var i in entry){
     roll.append("th").html(i);
   }
-  while(new Date(entry.date_submitted)>datebound){
+  while(new Date(entry.timeStarted)>datebound){
     roll =  table.append("tr");
     for(var i in entry){
       roll.append("td").html(entry[i]);
@@ -16,3 +16,13 @@ function settable(data){
     entry = tabledata.pop();
   }
 }
+
+
+$(document).ready(function() { 
+  connectSyncButton();
+
+  data = retrieveAllPlantData();
+  if (data.length > 0) {
+    settable(data);
+  }
+});
