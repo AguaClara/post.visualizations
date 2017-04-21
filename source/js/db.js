8f40734340b412ac95d7bfd864e759ec86baf8fa
Jay Castro <Jay Castro>
2017-04-21 13:09:22 -0400

    committing old changes before sync with relevant website features, layout updates, and bugfixes

--- source/js/db.js
+++ source/js/db.js
@@ -179,6 +179,7 @@ function getAllPlantsDict(){
 		"smat":"San Matias", 
 		"snic":"San Nicolas", 
 		"tam":"Tamara",
-		"lasv":"Las Vegas"
+		"lasv":"Las Vegas",
+		"sjg":"San Juan Guarita"
 	}
 };
